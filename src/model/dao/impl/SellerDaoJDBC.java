330bc3ad69ead2b22feb79638eed3aa095acd93e
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-06-07 13:08:01 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.mysql.cj.xdevapi.Result;
 
 import db.DB;
 import db.DbException;
@@ -25,7 +29,36 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
-        // TODO Auto-generated method stub
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    + "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                if(rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }else{
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
 
     }
 
@@ -92,10 +125,8 @@ public class SellerDaoJDBC implements SellerDao {
 
         try {
             st = conn.prepareStatement(
-                "SELECT seller.*,department.Name as DepName "
-                + "FROM seller INNER JOIN department "
-                + "ON seller.DepartmentId = department.Id "
-                + "ORDER BY Name");
+                    "SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id " + "ORDER BY Name");
 
             rs = st.executeQuery();
 
@@ -104,9 +135,9 @@ public class SellerDaoJDBC implements SellerDao {
 
             while (rs.next()) {
                 Department dep = map.get(rs.getInt("DepartmentId"));
-                if(dep == null){
+                if (dep == null) {
                     dep = instantiateDepartment(rs);
-                    map.put(rs.getInt("DepartmentId"),dep);
+                    map.put(rs.getInt("DepartmentId"), dep);
                 }
                 Seller obj = instantiateSeller(rs, dep);
                 list.add(obj);
@@ -127,11 +158,8 @@ public class SellerDaoJDBC implements SellerDao {
 
         try {
             st = conn.prepareStatement(
-                "SELECT seller.*,department.Name as DepName "
-                + "FROM seller INNER JOIN department "
-                + "ON seller.DepartmentId = department.Id "
-                + "WHERE DepartmentId = ? "
-                + "ORDER BY Name");
+                    "SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
 
             st.setInt(1, department.getId());
             rs = st.executeQuery();
@@ -141,9 +169,9 @@ public class SellerDaoJDBC implements SellerDao {
 
             while (rs.next()) {
                 Department dep = map.get(rs.getInt("DepartmentId"));
-                if(dep == null){
+                if (dep == null) {
                     dep = instantiateDepartment(rs);
-                    map.put(rs.getInt("DepartmentId"),dep);
+                    map.put(rs.getInt("DepartmentId"), dep);
                 }
                 Seller obj = instantiateSeller(rs, dep);
                 list.add(obj);
